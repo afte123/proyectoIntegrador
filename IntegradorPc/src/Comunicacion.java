f4393210e5283f32bd16e6e2a666808f2aa0db76
afte123 <afte-2222@hotmail.com>
2016-03-09 14:44:57 -0500

    Prueba

--- IntegradorPc/src/Comunicacion.java
+++ IntegradorPc/src/Comunicacion.java
@@ -8,6 +8,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 
+import comun.Mensaje;
 import processing.core.*;
 public class Comunicacion extends Thread {
 
@@ -15,6 +16,8 @@ public class Comunicacion extends Thread {
 	private DatagramSocket socket;
 	private DatagramPacket packet;
 	private String command="";
+	private byte[] buzon;
+	private DatagramPacket pRecibir;
 	
 	Comunicacion(){
 
@@ -24,7 +27,7 @@ public class Comunicacion extends Thread {
 			System.out.println("UDP socket open and waiting data");
 
 			// Create the buffer and the receiving packet
-			byte[] buffer = new byte[64];
+			byte[] buffer = new byte[2364];
 			packet = new DatagramPacket(buffer, buffer.length);
 
 		} catch (SocketException e) {
@@ -32,16 +35,26 @@ public class Comunicacion extends Thread {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	public Object recibir() throws IOException, ClassNotFoundException {
+		buzon = new byte[1025];
+		pRecibir = new DatagramPacket(buzon, buzon.length);
+		
+			socket.receive(pRecibir);
+			Object obj = deserializar(buzon);
+			System.out.println(buzon);
+			return obj;
+
+	}
 
 	public void run() {
 		
 		while (true) {
+			Mensaje mensaje;
 			try {
 				// Receive packets and process the information
-				
-
-
-				
+				mensaje = (Mensaje) recibir();
 				socket.receive(packet);
 				command= new String(packet.getData(),0,packet.getLength());
 				
@@ -52,6 +65,9 @@ public class Comunicacion extends Thread {
 				
 				
 			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
